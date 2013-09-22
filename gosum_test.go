d7bfda4912b58ae6b921f2a01697e73316473e56
mulander <netprobe@gmail.com>
2013-09-22 17:24:15 +0200

    Add a test for md5sum Check

--- gosum_test.go
+++ gosum_test.go
@@ -18,3 +18,16 @@ func TestAdd(t *testing.T) {
 		t.Error("md5sum.Add checksum mismatch")
 	}
 }
+
+func TestCheck(t *testing.T) {
+	md5sum := NewMD5Sum()
+	md5sum.Add("a.txt", bytes.NewReader([]byte("test")))
+
+	ok, err := md5sum.Check("a.txt", bytes.NewReader([]byte("test")))
+	if err != nil {
+		t.Error(err)
+	}
+	if !ok {
+		t.Error("md5sum.Add checksum mismatch")
+	}
+}
